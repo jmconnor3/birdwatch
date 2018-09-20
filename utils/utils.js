052ae9a7816ae1f632d859b682b68806096b67be
Connor <jmconnor3@gmail.com>
2018-09-19 19:42:25 -0500

    'added sciNmae function'

--- utils/utils.js
+++ utils/utils.js
@@ -6,7 +6,7 @@ make a function that makes a request to the ebird that obtains all of the recent
 in the metro new orleans area
 
 */
-/*TODO
+/* TODO
 Set env key for ebird call;
 */
 
@@ -23,4 +23,28 @@ const eBird = (callback) => {
   });
 };
 
+/*
+build a function that responds to a user input of a string
+that is a bird name and a string that is a location
+that then returns
+`{
+    "birdCommon": "test-bird",
+    "birdScience": "testus-birdus",
+    "user": "test",
+    "flockSize": 20,
+    "location": {
+        "lat": 30.000001,
+        "lng": -90.095701
+    }
+}
+
+*/
+const sciName = (name, callback) => {
+  request(`http://www.xeno-canto.org/api/2/recordings?query=${name}`, (err, response, body) => {
+    if (err) {
+      console.error(err);
+    } callback(err, response, body);
+  });
+};
+module.exports.sciName = sciName;
 module.exports.eBird = eBird;
