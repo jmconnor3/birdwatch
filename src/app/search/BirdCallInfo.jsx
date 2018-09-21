4e2ae4ee49953395195705673cc09f156c00849c
Connor <jmconnor3@gmail.com>
2018-09-21 12:01:09 -0500

    'made changes to info component for photo render'

--- src/app/search/BirdCallInfo.jsx
+++ src/app/search/BirdCallInfo.jsx
@@ -3,10 +3,11 @@ import { CardMedia } from 'material-ui';
 
 const BirdCallInfo = ({ props }) => {
   console.log(props[0]);
+  const key = props[0].imgs;
 
   return (
     <div>
-      <img src={props[0].imgs[0]} alt={props[0].sciName}/>
+      <img src={`//upload.wikimedia.org/wikipedia/commons/thumb/9/9d/${key}/220px-${key}`} alt={props[0].sciName}/>
       <h1>{props[0].sciName}</h1>
       <h1>{props[0].descript}</h1>
       <audio controls src={props[0].audio}></audio>
