a72ad68b3d68ce6fa5ef51c4342ea023f2984f30
Connor <jmconnor3@gmail.com>
2018-09-21 10:55:52 -0500

    ' have bird calls rendering and playing'

--- src/app/search/BirdCallInfo.jsx
+++ src/app/search/BirdCallInfo.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactAudioPlayer from 'react-audio-player';
 import { CardMedia } from 'material-ui';
 
 const BirdCallInfo = ({ props }) => {
-  console.log();
+  console.log(props[0]);
 
   return (
     <div>
-      <CardMedia>
-        <img src={props[0].imgs[0].substring(5)} />
-      </CardMedia>
+      <img src={props[0].imgs[0].substring(5)} />
       <h1>{props[0].sciName}</h1>
       <h1>{props[0].descript}</h1>
-      {/* <ReactAudioPlayer src={props.audio} controls crossOrigin /> */}
-      <h1>{props[0].imgs[0]}</h1>
+      <audio controls src={props[0].audio}></audio>
+  
     </div>
   );
 };
